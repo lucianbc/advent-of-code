16df5326f54a048454db2774d2a3aa29d0bf47a2
lucianbc <lucianboaca97@gmail.com>
2021-01-23 19:26:50 +0000

    Day 21 - remove useless code

--- src/main/kotlin/Day21.kt
+++ src/main/kotlin/Day21.kt
@@ -43,11 +43,6 @@ object Day21 {
     
     
     data class Recipe(val ingredients: Set<String>, val alergens: Set<String>)
-    enum class TheoremResponse {
-        Proven,
-        False,
-        Undecidable
-    }
     
     fun intersectAlergens(recipes: List<Recipe>) = recipes
         .flatMap { r ->
@@ -92,54 +87,5 @@ object Day21 {
         }
         
         return mappings
-    }
-    
-    /*
-    Treat recipes like evidences. A theorem is something like "ingredient x is alergen y".
-    To prove this theorem, I first filter all the recipes to find the ones containing that ingredient.
-    Then:
-      - the theorem is true if it satisfies all the recipes AND there is at least one recipe where the
-            only alergen is the one I am proving and that rule does not contain any other ingredient so far
-      - the theorem is false if there is one recipe that contains the ingredient, but doesn't contain the alergen
-      - by default it is undecidable
-     */
-    fun prove(ingredient: String, alergen: String, recipes: List<Recipe>): TheoremResponse {
-        
-        val initialReduceValue = Triple(
-            emptyList<Recipe>(), // recipes that contain the ingredient and multiple alergens
-            emptyList<Recipe>(), // recipes tht contain the ingredient and a single alergen
-            emptyList<Recipe>(), // recipes that contain the ingredient, but not the alergen or the alergen, but not the ingredient
-        )
-        
-        val (_, singleAlergen, contradictions) = recipes.fold(initialReduceValue) { acc, crt ->
-            val (multiAlergens, singleAlergen, contradictions) = acc
-            
-            val crtHasAlergen = crt.alergens.contains(alergen)
-            val crtHasIngredient = crt.ingredients.contains(ingredient)
-            
-            if (crtHasAlergen && crtHasIngredient) {
-                if (crt.alergens.size == 1) {
-                    Triple(multiAlergens, singleAlergen.plus(crt), contradictions)
-                } else {
-                    Triple(multiAlergens.plus(crt), singleAlergen, contradictions)
-                }
-            } else if (!crtHasAlergen && !crtHasIngredient) {
-                acc
-            } else {
-                Triple(multiAlergens, singleAlergen, contradictions.plus(crt))
-            }
-        }
-        
-        return when {
-            contradictions.isNotEmpty() -> {
-                TheoremResponse.False
-            }
-            singleAlergen.isNotEmpty() -> {
-                TheoremResponse.Proven
-            }
-            else -> {
-                TheoremResponse.Undecidable
-            }
-        }
     }
 }
