6634e6f0bdcee88b39419c5324cf1ca9a4a32458
lucianbc <lucianboaca97@gmail.com>
2021-01-30 22:29:46 +0000

    Day 24 - attempt 2 - failure

--- src/main/kotlin/Day24.kt
+++ src/main/kotlin/Day24.kt
@@ -15,8 +15,23 @@ object Day24 {
 //            .mapValues { it.value.count() }
 
 //        println(r)
+
+//        performDirections(parseLine("nwwsw")).also { println(it) }
+
+//        read()
+//            .take(8)
+//            .forEach { Swarm.identifyNew(it) }
+
+        sequenceOf(
+            sequenceOf(Direction.East, Direction.East, Direction.East),
+            sequenceOf(Direction.SouthEast, Direction.SouthEast, Direction.East, Direction.NorthEast, Direction.NorthEast),
+//            sequenceOf(Direction.SouthWest),
+//            sequenceOf(Direction.West),
+//            sequenceOf(Direction.NorthWest),
+//            sequenceOf(Direction.NorthEast),
+        ).forEach { Swarm.identifyNew(it) }
         
-        performDirections(parseLine("nwwsw")).also { println(it) }
+        println(Swarm.countAll())
     }
     
     fun performDirections(directions: Sequence<Direction>) =
@@ -58,5 +73,71 @@ object Day24 {
                 Direction.NorthWest -> Tile(axis1, axis2 - 1, axis3)
                 Direction.NorthEast -> Tile(axis1, axis2, axis3 - 1)
             }
+    }
+    
+    object Swarm {
+        var tileId = 0
+        class Tile(
+            var turns: Int = 0,
+            var e: Tile? = null,
+            var se: Tile? = null,
+            var sw: Tile? = null,
+            var w: Tile? = null,
+            var nw: Tile? = null,
+            var ne: Tile? = null,
+        ) {
+            val it = tileId++
+            /*
+            Prime example of where 'pass-by-reference' would help. If I had a way to pass the pointer by reference,
+            I could have modified it generically, saving me 5 lines basically
+             */
+            fun goTo(direction: Direction): Tile = when (direction) {
+                Direction.East -> e ?: Tile().also {
+                    e = it; it.w = this
+                    it.sw = se; se?.ne = it
+                    it.nw = ne; ne?.se = it
+                }
+                Direction.SouthEast -> se ?: Tile().also {
+                    se = it; it.nw = this
+                    it.ne = e; e?.sw = it
+                    it.w = sw; sw?.e = it
+                }
+                Direction.SouthWest -> sw ?: Tile().also {
+                    sw = it; it.ne = this
+                    it.e = se; se?.w = it
+                    it.nw = w; w?.se = it
+                }
+                Direction.West -> w ?: Tile().also {
+                    w = it; it.e = this
+                    it.se = sw; sw?.nw = it
+                    it.ne = nw; nw?.sw = it
+                }
+                Direction.NorthWest -> nw ?: Tile().also {
+                    nw = it; it.se = this
+                    it.e = ne; ne?.w = it
+                    it.sw = w; w?.ne = it
+                }
+                Direction.NorthEast -> ne ?: Tile().also {
+                    ne = it; it.sw = this
+                    it.se = e; e?.nw = it
+                    it.w = nw; nw?.e = it
+                }
+            }
+        }
+        
+        private val destinations = HashSet<Tile>()
+        
+        private val reference = Tile()
+        
+        fun identifyNew(directions: Sequence<Direction>) {
+            directions.fold(reference) { acc, crt -> acc.goTo(crt) }
+                .also { it.turns += 1; destinations.add(it) }
+        }
+        
+        fun countAll(): Map<Int, Int> {
+            return destinations
+                .groupBy { it.turns }
+                .mapValues { it.value.size }
+        }
     }
 }
