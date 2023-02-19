6144fb4d2236d35b8b318e053e6340b1268e9a38
lucianbc <lucianboaca97@gmail.com>
2023-02-19 11:53:52 +0000

    Day 16 - slow

--- 2022/src/main/kotlin/Day-16.kt
+++ 2022/src/main/kotlin/Day-16.kt
@@ -1,7 +1,7 @@
 import java.lang.Integer.max
 
 fun main() {
-  Day16.part1()
+  Day16.part2()
 }
 
 object Day16 {
@@ -33,7 +33,165 @@ object Day16 {
       }
     }
 
-    private val memo = mutableMapOf<Pair<String, Int>, Int>()
+    data class MemoKeyPart22(
+      val nodeKey: String,
+      val timeLeft: Int,
+      val openedSoFar: Set<String>,
+      val remainingPlayers: Int,
+    )
+
+    private val memo2part22 = mutableMapOf<MemoKeyPart22, Int>()
+
+    fun maxScore2(
+      nodeKey: String,
+      timeLeft: Int,
+      openedSoFar: Set<String>,
+      pathSoFar: Set<String>,
+      remainingPlayers: Int
+    ): Int {
+      val memoKey = MemoKeyPart22(
+        nodeKey,
+        timeLeft,
+        openedSoFar,
+        remainingPlayers,
+      )
+
+      if (memo2part22.containsKey(memoKey)) {
+        return memo2part22[memoKey]!!
+      }
+
+
+      // 24614453 iterations
+      // correct response 2400
+      println("Computing ${stepCount++} $nodeKey $timeLeft $openedSoFar $pathSoFar")
+
+      if (timeLeft == 0) {
+        return if (remainingPlayers > 0) {
+          maxScore2("AA", 26, openedSoFar, emptySet(), remainingPlayers - 1)
+        } else {
+          0
+        }
+      }
+
+      var result = 0
+
+      val node = nodes[nodeKey]!!
+
+      if (openedSoFar.contains(nodeKey).not() && node.flow != 0) {
+        result = max(
+          result,
+          (timeLeft - 1) * node.flow + maxScore2(
+            nodeKey,
+            timeLeft - 1,
+            openedSoFar + setOf(nodeKey),
+            emptySet(),
+            remainingPlayers
+          )
+        )
+      }
+
+      node.links.forEach { link ->
+//        if (pathSoFar.contains(link).not()) {
+          result = max(result, maxScore2(link, timeLeft - 1, openedSoFar, pathSoFar + setOf(link), remainingPlayers))
+//        }
+      }
+      memo2part22[memoKey] = result
+      return result
+    }
+
+    data class MemoPart2Key(
+      val nodeKey: String,
+      val otherNodeKey: String,
+      val timeLeft: Int,
+      val openedSoFar: Set<String>,
+    )
+
+    private val memoPart2 = mutableMapOf<MemoPart2Key, Int>()
+
+    fun maxScorePart2(nodeKey: String, otherNodeKey: String, timeLeft: Int, openedSoFar: Set<String>, path1SoFar: Set<String>, path2SoFar: Set<String>): Int {
+      val memoKey = MemoPart2Key(
+        nodeKey,
+        otherNodeKey,
+        timeLeft,
+        openedSoFar,
+      )
+
+      if (memoPart2.containsKey(memoKey)) {
+        println("Cache hit $nodeKey $otherNodeKey $timeLeft")
+        return memoPart2[memoKey]!!
+      }
+
+      if (timeLeft == 0) {
+        return 0
+      }
+
+      var result = 0
+
+      fun worthOpening(key: String): Boolean {
+        return openedSoFar.contains(key).not() && nodes[key]!!.flow != 0
+      }
+
+      if (worthOpening(nodeKey) && worthOpening(otherNodeKey) && nodeKey != otherNodeKey) {
+        result =
+          max(
+            (nodes[nodeKey]!!.flow + nodes[otherNodeKey]!!.flow) * (timeLeft - 1) + maxScorePart2(
+              nodeKey,
+              otherNodeKey,
+              timeLeft - 1,
+              openedSoFar + setOf(nodeKey, otherNodeKey),
+              emptySet(),
+              emptySet(),
+            ),
+            result
+          )
+      }
+      if (worthOpening(nodeKey)) {
+        nodes[otherNodeKey]!!.links.forEach { otherLink ->
+          if (path2SoFar.contains(otherLink).not()) {
+            result = max(
+              result,
+              nodes[nodeKey]!!.flow * (timeLeft - 1) + maxScorePart2(
+                nodeKey,
+                otherLink,
+                timeLeft - 1,
+                openedSoFar + setOf(nodeKey),
+                emptySet(),
+                path2SoFar + setOf(otherLink)
+              )
+            )
+          }
+        }
+      }
+      if (worthOpening(otherNodeKey)) {
+        nodes[nodeKey]!!.links.forEach { link ->
+          if (path1SoFar.contains(link).not()) {
+            result = max(
+              result,
+              nodes[otherNodeKey]!!.flow * (timeLeft - 1) + maxScorePart2(
+                link,
+                otherNodeKey,
+                timeLeft - 1,
+                openedSoFar + setOf(otherNodeKey),
+                path1SoFar + setOf(link),
+                emptySet()
+              )
+            )
+          }
+        }
+      }
+      nodes[nodeKey]!!.links.forEach { link ->
+        nodes[otherNodeKey]!!.links.forEach { otherLink ->
+          result = max(result, maxScorePart2(link, otherLink, timeLeft - 1, openedSoFar,
+            path1SoFar + setOf(link), path2SoFar + setOf(otherLink)
+            ))
+        }
+      }
+
+
+      memoPart2[memoKey] = result
+
+      return result
+    }
 
     data class MemoKey(
       val nodeKey: String,
@@ -46,7 +204,6 @@ object Day16 {
 
     private var stepCount = 0
     fun maxScore2(nodeKey: String, timeLeft: Int, openedSoFar: Set<String>, pathSoFar: Set<String>): Int {
-      println("Computing ${stepCount++} $nodeKey $timeLeft $openedSoFar $pathSoFar")
       val memoKey = MemoKey(
         nodeKey,
         timeLeft,
@@ -56,6 +213,9 @@ object Day16 {
       if (memo2.containsKey(memoKey)) {
         return memo2[memoKey]!!
       }
+      // 963946 iterations on real input
+      // 861610 iterations with path so far optimisation
+      println("Computing ${stepCount++} $nodeKey $timeLeft $openedSoFar $pathSoFar")
 
       if (timeLeft == 0) {
         return 0
@@ -66,66 +226,57 @@ object Day16 {
       val node = nodes[nodeKey]!!
 
       if (openedSoFar.contains(nodeKey).not() && node.flow != 0) {
-        result = max(result, (timeLeft - 1) * node.flow + maxScore2(nodeKey, timeLeft - 1, openedSoFar + setOf(nodeKey), pathSoFar))
+        result = max(
+          result,
+          (timeLeft - 1) * node.flow + maxScore2(nodeKey, timeLeft - 1, openedSoFar + setOf(nodeKey), emptySet())
+        )
       }
 
       node.links.forEach { link ->
-        result = max(result, maxScore2(link, timeLeft - 1, openedSoFar, pathSoFar + setOf(link)))
+        if (pathSoFar.contains(link).not()) {
+          result = max(result, maxScore2(link, timeLeft - 1, openedSoFar, pathSoFar + setOf(link)))
+        }
       }
-
-
-//      val result = if (timeLeft <= 1) {
-//        0
-//      } else {
-//        val node = nodes[nodeKey]!!
-//
-//        val nodeFlow = (timeLeft - 1) * node.flow
-//
-//        val maxWithoutOpeningThis = node.links
-//          .filter { pathSoFar.contains(it).not() }
-//          .maxOfOrNull { maxScore2(it, timeLeft - 1, openedSoFar, pathSoFar + setOf(nodeKey)) } ?: if (openedSoFar.contains(nodeKey)) 0 else nodeFlow
-//
-//        if (openedSoFar.contains(node.name) || node.flow == 0) {
-//          maxWithoutOpeningThis
-//        } else {
-//          val maxWithOpeningThis = node.links.map { maxScore2(it, timeLeft - 2, openedSoFar + listOf(nodeKey), emptySet()) }.max() + nodeFlow
-//          max(maxWithoutOpeningThis, maxWithOpeningThis)
-//        }
-//      }
       memo2[memoKey] = result
       return result
     }
 
-    fun maxScore(nodeKey: String, timeLeft: Int): Int {
-      val memoKey = nodeKey to timeLeft
-//      println("Running for $nodeKey and $timeLeft")
-      if (memo.containsKey(memoKey)) {
-        return memo[memoKey]!!
+    fun maxScore(nodeKey: String, timeLeft: Int, openedSoFar: Set<String>, pathSoFar: Set<String>): Int {
+      val memoKey = MemoKey(
+        nodeKey,
+        timeLeft,
+        openedSoFar,
+      )
+
+      if (memo2.containsKey(memoKey)) {
+        return memo2[memoKey]!!
       }
+
+      // 1604907 iterations on real input
+      println("Computing ${stepCount++} $nodeKey $timeLeft $openedSoFar $pathSoFar")
 
       val result = if (timeLeft <= 1) {
         0
       } else {
-        nodes[nodeKey]!!.let { node ->
-          val links = node.links
-          val maxWithoutThis = links.maxOfOrNull { maxScore(it, timeLeft - 1) }!!
+        val node = nodes[nodeKey]!!
 
-          if (node.flow == 0) {
-            maxWithoutThis
-          } else {
-            val scoreOfThis = node.flow * (timeLeft - 1)
-            val maxWithThis = links.maxOfOrNull { maxScore(it, timeLeft - 2) }!!
+        val nodeFlow = (timeLeft - 1) * node.flow
 
-            if (scoreOfThis + maxWithThis > maxWithThis) {
-              println("Open ${node.name} for ${timeLeft - 1} minutes")
-            }
+        val maxWithoutOpeningThis = node.links
+          .filter { pathSoFar.contains(it).not() }
+          .maxOfOrNull { maxScore2(it, timeLeft - 1, openedSoFar, pathSoFar + setOf(nodeKey)) }
+          ?: if (openedSoFar.contains(nodeKey)) 0 else nodeFlow
 
-            max(maxWithThis, maxWithThis + scoreOfThis)
-          }
+        if (openedSoFar.contains(node.name) || node.flow == 0) {
+          maxWithoutOpeningThis
+        } else {
+          val maxWithOpeningThis =
+            node.links.map { maxScore2(it, timeLeft - 2, openedSoFar + listOf(nodeKey), emptySet()) }.max() + nodeFlow
+          max(maxWithoutOpeningThis, maxWithOpeningThis)
         }
       }
 
-      memo[memoKey] = result
+      memo2[memoKey] = result
       return result
     }
   }
@@ -136,12 +287,15 @@ object Day16 {
     input.forEach {
       graph.parseNode(it)
     }
-    println(graph.maxScore2(graph.firstNode, 30, emptySet(), emptySet()))
-    // println("Result is $result")
+    println(graph.maxScore2("AA", 30, emptySet(), emptySet()))
   }
 
   fun part2() {
     val input = DataReader.read(16)
-    // println("Result is $result")
+    val graph = GraphMap()
+    input.forEach {
+      graph.parseNode(it)
+    }
+    println(graph.maxScore2("AA",  26, emptySet(), emptySet(), 1))
   }
 }
---
+++ 2022/src/main/kotlin/Search.kt
@@ -0,0 +1,273 @@
+import java.util.*
+
+/**
+ * Various common path finding algorithms. Based on https://www.redblobgames.com/pathfinding/a-star/introduction.html
+ * and https://www.redblobgames.com/pathfinding/a-star/implementation.html which explains BFS, Djikstra and A*
+ * in an easy-to-understand way.
+ *
+ * Which algorithm to use when:
+ * - Use BFS if there is no cost involved, or the whole graph have to be searched.
+ * - Use Djikstra if there is a different cost depending on how you move, but there is no way
+ *   to estimate the remaining cost.
+ * - Use A* if there is cost involved, and it's possible estimate the remaining cost without giving an
+ *   over estimation. With an over estimation the shortest path is not guaranteed to be found.
+ * - Use maximizeValueDfs when a maximum value/cost should be found rather than the minimal, and it is possible
+ *   to estimate the final value without doing an underestimation. This is a DFS algorithm so with a good estimate
+ *   it's possible to cut off many non-viable branches very early on and find the optimal result quickly.
+ *
+ * How to use:
+ * - Create a class that implement the suitable Graph interface depending on algorithm
+ * - The class holds the representation of the thing to search through and just provides the available neighbours
+ *   and possible cost for any given node.
+ * - Call the desired search function with the provided graph.
+ */
+object Search {
+  /**
+   * Interface that should be implemented by a class to do BFS. To search through a AMap, which uses Pos for
+   * indexing, create a class that implements Graph<Pos> and pass that to the bfs() function.
+   */
+  interface Graph<T> {
+    /**
+     * Returns all neighbours that it's possible to reach from the given node id.
+     */
+    fun neighbours(id: T): List<T>
+  }
+
+  /**
+   * Interface that should be implemented by a class when Djikstra or A* search is wanted.
+   */
+  interface WeightedGraph<T> : Graph<T> {
+    /**
+     * The cost for moving between the two given nodes.
+     *
+     * Note, since cost returns a float, make sure to return fractions that are powers of two if it's important
+     * to avoid rounding errors. For example when it's important which path is taken if two paths have exactly
+     * the same cost (for example WWSS vs SSWW)
+     */
+    fun cost(from: T, to: T): Float
+  }
+
+  /**
+   * Holds the result of a search.
+   * @param cameFrom shows how to get to a certain location. "cameFrom[B] == A" means that the path
+   * to B comes from A. The start point points back to itself, so  cameFrom[A] == A.
+   * @param cost a map that shows the total cost to get to all visited locations
+   */
+  data class Result<T>(val cameFrom: Map<T, T>, val cost: Map<T, Float>) {
+    /**
+     * Gives the best path to the given location from the start point of the search (start point
+     * not included in the returned path).
+     */
+    fun getPath(to: T): List<T> {
+      return buildList {
+        var current = to
+        while (cameFrom[current] != null && cameFrom[current] != current) {
+          add(current)
+          current = cameFrom.getValue(current)
+        }
+      }.asReversed()
+    }
+  }
+
+  /**
+   * Standard breadth first search. Good when all locations must be visited or when there is no cost
+   * involved and there is no reasonable heuristic that can be used to find the distance to the goal.
+   * @param graph the graph to search
+   * @param start the location to start the search from
+   * @param goal the location to search for, or null if the whole graph should be searched
+   */
+  fun <T> bfs(graph: Graph<T>, start: T, goal: T?): Result<T> {
+    val toCheck = ArrayDeque<T>()
+    toCheck.add(start)
+    val cameFrom = mutableMapOf(start to start)
+    while (toCheck.isNotEmpty()) {
+      val current = toCheck.removeFirst()
+      if (current == goal) {
+        break
+      }
+      for (next in graph.neighbours(current)) {
+        if (next !in cameFrom) {
+          toCheck.add(next)
+          cameFrom[next] = current
+        }
+      }
+    }
+    return Result(cameFrom, mapOf())
+  }
+
+  /**
+   * Good search algorithm when there is different cost involved in moving to different locations,
+   * and there is no good heuristic for estimating the remaining cost to the goal.
+   * @param graph the graph to search
+   * @param start the location to start the search from
+   * @param goal the location to search for
+   * @param maxCost the maximum cost allowed to find a solution. No cost limit applied if not provided.
+   */
+  fun <T> djikstra(graph: WeightedGraph<T>, start: T, goal: T, maxCost: Float = Float.MAX_VALUE): Result<T> {
+    val toCheck = PriorityQueue(compareBy<Pair<T, Float>> { it.second })
+    toCheck.add(start to 0f)
+    val cameFrom = mutableMapOf(start to start)
+    val costSoFar = mutableMapOf(start to 0.0f)
+    while (toCheck.isNotEmpty()) {
+      val (current, _) = toCheck.remove()
+      if (current == goal) {
+        break
+      }
+      for (next in graph.neighbours(current)) {
+        val nextCost = costSoFar.getValue(current) + graph.cost(current, next)
+        if (nextCost < maxCost && nextCost < costSoFar.getOrDefault(next, Float.MAX_VALUE)) {
+          toCheck.add(next to nextCost)
+          cameFrom[next] = current
+          costSoFar[next] = nextCost
+        }
+      }
+    }
+    return Result(cameFrom, costSoFar)
+  }
+
+  // Note: In both djikstra and a* the toCheck queue may have multiple entries of the same location but with different
+  // cost. There is no additional early continue for those as all their neighbours will be more expensive than
+  // what's been processed so far anyway so that path will lead to a dead end quickly.
+  // This is also mentioned as point 5 in https://www.redblobgames.com/pathfinding/a-star/implementation.html#algorithm
+
+  /**
+   * A* is a good algorithm for finding the shortest path when there is cost involved, and it's possible to use
+   * a heuristic to estimate the cost to get to the goal. The heuristic must be admissible (never overestimate
+   * the cost). If the heuristic overestimates the cost it will not be guaranteed to find the shortest path.
+   * @param graph the graph to search
+   * @param start the location to start the search from
+   * @param goal the location to search for
+   * @param heuristic a heuristic function that estimates the cost to reach the goal. Parameters are start location
+   * and goal location while the return value is the estimated cost to move from start to goal. Must never
+   * overestimate the cost to reach the goal.
+   * @param maxCost the maximum cost allowed to find a solution. No cost limit applied if not provided.
+   */
+  fun <T> aStar(
+    graph: WeightedGraph<T>,
+    start: T,
+    goal: T,
+    heuristic: (T, T) -> Float,
+    maxCost: Float = Float.MAX_VALUE
+  ): Result<T> {
+    val toCheck = PriorityQueue(compareBy<Pair<T, Float>> { it.second })
+    toCheck.add(start to 0f)
+    val cameFrom = mutableMapOf(start to start)
+    val costSoFar = mutableMapOf(start to 0.0f)
+    while (toCheck.isNotEmpty()) {
+      val (current, _) = toCheck.remove()
+      if (current == goal) {
+        break
+      }
+      for (next in graph.neighbours(current)) {
+        val nextCost = costSoFar.getValue(current) + graph.cost(current, next)
+        if (nextCost < maxCost && nextCost < costSoFar.getOrDefault(next, Float.MAX_VALUE)) {
+          toCheck.add(next to nextCost + heuristic(next, goal))
+          cameFrom[next] = current
+          costSoFar[next] = nextCost
+        }
+      }
+    }
+    return Result(cameFrom, costSoFar)
+  }
+
+  /**
+   * A* but rather than searching for a known goal position it takes a goal function and keep searching until
+   * the goal function returns true. The cost map in the result may contain many nodes that fulfill the goal
+   * function as nodes are inserted into the cost map when they are queued for continued searching. The returned
+   * node is the optimal one that triggered the goal function first.
+   *
+   * @param graph the graph to search
+   * @param start the location to start the search from
+   * @param goalFn a function that takes a node and should return true when the goal is found.
+   * @param heuristic a heuristic function that estimates the cost to reach the goal. Start location
+   * is given as parameter and the return value is the estimated cost to move from start to goal. Must never
+   * overestimate the cost to reach the goal.
+   * @param maxCost the maximum cost allowed to find a solution. No cost limit applied if not provided.
+   * @return A pair of the node that triggered the goal function to return true to the search Result. If the goal
+   *         isn't found the start node is returned instead.
+   */
+  fun <T> aStar(
+    graph: WeightedGraph<T>,
+    start: T,
+    goalFn: (T) -> Boolean,
+    heuristic: (T) -> Float,
+    maxCost: Float = Float.MAX_VALUE
+  ): Pair<T, Result<T>> {
+    val toCheck = PriorityQueue(compareBy<Pair<T, Float>> { it.second })
+    toCheck.add(start to 0f)
+    val cameFrom = mutableMapOf(start to start)
+    val costSoFar = mutableMapOf(start to 0.0f)
+    while (toCheck.isNotEmpty()) {
+      val (current, _) = toCheck.remove()
+      if (goalFn(current)) {
+        return current to Result(cameFrom, costSoFar)
+      }
+      for (next in graph.neighbours(current)) {
+        val nextCost = costSoFar.getValue(current) + graph.cost(current, next)
+        if (nextCost < maxCost && nextCost < costSoFar.getOrDefault(next, Float.MAX_VALUE)) {
+          toCheck.add(next to nextCost + heuristic(next))
+          cameFrom[next] = current
+          costSoFar[next] = nextCost
+        }
+      }
+    }
+    return start to Result(cameFrom, costSoFar)
+  }
+
+  /**
+   * Search the given graph using a DFS approach in a way that maximises the value (rather than minimizing
+   * the cost).
+   *
+   * @param graph The graph to search, cost is treated as value that should be maximised
+   * @param start The start node
+   * @param isGoal Function that returns true if the state it's given is a goal state
+   * @param estimateBestCase Function that estimates the final value given a state. It must never underestimate
+   *        the actual value. If it does the best solution might not be found.
+   * @return A pair of best state to Result which holds information about all visited nodes during the search.
+   */
+  fun <T> maximizeValueDfs(
+    graph: WeightedGraph<T>,
+    start: T,
+    isGoal: (T) -> Boolean,
+    estimateBestCase: (T) -> Float,
+  ): Pair<T, Result<T>> {
+    val cameFrom = mutableMapOf(start to start)
+    val costSoFar = mutableMapOf(start to 0f)
+    val ret = dfs(graph, start, isGoal, estimateBestCase, start to 0f, cameFrom, costSoFar)
+    return ret.first to Result(cameFrom, costSoFar)
+  }
+
+  /**
+   * Use recursive dfs to find the node with the highest value (cost). Since a heuristic is used to prune
+   * branches that has no chance of beating the current best a recursive approach is better (faster) than an
+   * iterative approach. This is because an iterative approach will have to enqueue a lot of states that could
+   * otherwise be pruned away while it's iterating toward maximum depth.
+   *
+   * @return a pair of best state to the value (cost) at that state.
+   */
+  private fun <T> dfs(
+    graph: WeightedGraph<T>,
+    current: T,
+    isGoal: (T) -> Boolean,
+    estimateBestCase: (T) -> Float,
+    previousBest: Pair<T, Float>,
+    cameFrom: MutableMap<T, T>,
+    costSoFar: MutableMap<T, Float>
+  ): Pair<T, Float> {
+    if (isGoal(current)) {
+      return current to costSoFar.getValue(current)
+    }
+    var newBest = previousBest
+    graph.neighbours(current)
+      .filter { estimateBestCase(it) > newBest.second } // Only examine states that can beat current best
+      .forEach { next ->
+        cameFrom[next] = current
+        costSoFar[next] = graph.cost(current, next)
+        val ret = dfs(graph, next, isGoal, estimateBestCase, newBest, cameFrom, costSoFar)
+        if (ret.second > newBest.second) {
+          newBest = ret
+        }
+      }
+    return newBest
+  }
+}
